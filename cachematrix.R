98ac1b07228dfaa6092729bc54112bbf79dddee9
Steven Smith <SteveSmith6478@gmail.com>
2019-07-21 14:11:13 -0500

    These functions cache the inverse of a matrix in an object in an environment which is different from the current environment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
+## These functions cache the inverse of a matrix in an object in an environment
+## which is different from the current environment.
+
+## This function creates a special "matrix" object that can cache its inverse.
 ## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
+    m <- x$getinverse()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data,...)
+    x$setinverse(m)
+    m
         ## Return a matrix that is the inverse of 'x'
 }
